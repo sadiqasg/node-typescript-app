4f601ab37fc64ec73861d470a1b991cf28e25e3e
saslamp <sadiqasg@gmail.com>
2022-03-17 19:08:20 +0100

    Bi-Weekly challenge: simple node CRUD application

---
+++ src/routes/usersRoute.ts
@@ -0,0 +1,78 @@
+import express, { Response, Request } from 'express';
+const users = express.Router();
+
+type User = {
+  id: number;
+  name: string;
+  occupation: string;
+};
+
+const usersArray: Array<User> = [
+  {
+    id: 1,
+    name: 'sadiq',
+    occupation: 'Software Engineer',
+  },
+  {
+    id: 2,
+    name: 'fati',
+    occupation: 'Product Designer',
+  },
+];
+
+// get all users
+users.get('/', (req: Request, res: Response) => {
+  if (usersArray.length < 1) {
+    res.json({ message: 'No users available' });
+  } else {
+    res.json({ message: usersArray });
+  }
+});
+
+// get single user
+users.get('/:id', (req: Request, res: Response) => {
+  if (usersArray.length < 1) {
+    return res.json({ message: 'No users available' });
+  }
+  const id = req.params.id;
+  const singleUser = usersArray.filter((user) => {
+    if (user.id === Number(id)) return user;
+  });
+  res.json({ message: singleUser });
+});
+
+// post add user
+users.post('/', (req: Request, res: Response) => {
+  const newUser: User = req.body;
+  if (usersArray.length < 1) {
+    return res.json({ message: 'No users available' });
+  }
+  usersArray.push(newUser);
+  res.json({ message: 'New user added', newUser, usersArray });
+});
+
+// edit user
+users.put('/:id', (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+
+  if (usersArray.length < 1) {
+    return res.json({ message: 'No users available' });
+  }
+
+  const userUpdate = req.body;
+
+  const foundIndex = usersArray.findIndex((element) => element.id === id);
+  usersArray.splice(foundIndex, 1, userUpdate);
+
+  res.json({ message: 'update successful', userUpdate, usersArray });
+});
+
+// delete user
+users.delete('/:id', (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const foundIndex = usersArray.findIndex((element) => element.id === id);
+  usersArray.splice(foundIndex, 1);
+  res.json({ message: 'user deleted!', usersArray });
+});
+
+export default users;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from 'express';
+import usersRoute from './routes/usersRoute';
 
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
+
+app.use('/users', usersRoute);
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'API v1' });
