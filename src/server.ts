4b2a1f63276382d832c71b1aafba5d4e27841128
saslamp <sadiqasg@gmail.com>
2022-03-19 12:28:12 +0100

    converting csv file to json

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,25 @@
 import express, { Request, Response } from 'express';
+import csv from 'csvtojson';
 
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
+const csvFilePath = './src/myfile.csv';
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'API v1' });
+});
+
+app.get('/convert', async (req: Request, res: Response) => {
+  const jsonFile = await csv().fromFile(csvFilePath);
+  jsonFile.filter((i) => {
+    if (i.phone === undefined) {
+      i.phone = 'missing data';
+    }
+  });
+  res.json({ status: 200, message: jsonFile });
 });
 
 app.get('*', (req: Request, res: Response) => {
